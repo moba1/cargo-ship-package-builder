0a12b747a4f4fad31335453d8307c107ed1a2646
moba1 <moba2@protonmail.com>
2021-12-28 14:57:10 +0900

    add gawk

--- cross-tool-chain/src/roles/tools/main.bash
+++ cross-tool-chain/src/roles/tools/main.bash
@@ -4,60 +4,68 @@ set -e
 
 SOURCE_DIR="$( dirname -- "${BASH_SOURCE[0]}" )"
 
+# python3 \
+#   "$SOURCE_DIR/m4.py" \
+#   --dist-dir "$DIST_DIR" \
+#   --version "$M4_VERSION" \
+#   --install-dir "$WORK_ROOT" \
+#   --target "$TARGET" \
+#   --prefix "/usr"
+
+# python3 \
+#   "$SOURCE_DIR/ncurses.py" \
+#   --dist-dir "$DIST_DIR" \
+#   --version "$NCURSES_VERSION" \
+#   --install-dir "$WORK_ROOT" \
+#   --target "$TARGET" \
+#   --prefix "/usr" \
+#   --man-dir "/usr/share/man"
+
+# python3 \
+#   "$SOURCE_DIR/bash.py" \
+#   --dist-dir "$DIST_DIR" \
+#   --version "$BASH_VERSION_" \
+#   --install-dir "$WORK_ROOT" \
+#   --target "$TARGET" \
+#   --prefix "/usr"
+
+# python3 \
+#   "$SOURCE_DIR/coreutils.py" \
+#   --dist-dir "$DIST_DIR" \
+#   --version "$COREUTILS_VERSION" \
+#   --install-dir "$WORK_ROOT" \
+#   --target "$TARGET" \
+#   --prefix "/usr"
+
+# python3 \
+#   "$SOURCE_DIR/diffutils.py" \
+#   --dist-dir "$DIST_DIR" \
+#   --version "$DIFFUTILS_VERSION" \
+#   --install-dir "$WORK_ROOT" \
+#   --target "$TARGET" \
+#   --prefix "/usr"
+
+# python3 \
+#   "$SOURCE_DIR/file.py" \
+#   --dist-dir "$DIST_DIR" \
+#   --version "$FILE_VERSION" \
+#   --install-dir "$WORK_ROOT" \
+#   --target "$TARGET" \
+#   --prefix "/usr"
+
+# python3 \
+#   "$SOURCE_DIR/findutils.py" \
+#   --dist-dir "$DIST_DIR" \
+#   --version "$FINDUTILS_VERSION" \
+#   --install-dir "$WORK_ROOT" \
+#   --target "$TARGET" \
+#   --prefix "/usr" \
+#   --localstatedir "/var/lib/located"
+
 python3 \
-  "$SOURCE_DIR/m4.py" \
+  "$SOURCE_DIR/gawk.py" \
   --dist-dir "$DIST_DIR" \
-  --version "$M4_VERSION" \
+  --version "$GAWK_VERSION" \
   --install-dir "$WORK_ROOT" \
   --target "$TARGET" \
   --prefix "/usr"
-
-python3 \
-  "$SOURCE_DIR/ncurses.py" \
-  --dist-dir "$DIST_DIR" \
-  --version "$NCURSES_VERSION" \
-  --install-dir "$WORK_ROOT" \
-  --target "$TARGET" \
-  --prefix "/usr" \
-  --man-dir "/usr/share/man"
-
-python3 \
-  "$SOURCE_DIR/bash.py" \
-  --dist-dir "$DIST_DIR" \
-  --version "$BASH_VERSION_" \
-  --install-dir "$WORK_ROOT" \
-  --target "$TARGET" \
-  --prefix "/usr"
-
-python3 \
-  "$SOURCE_DIR/coreutils.py" \
-  --dist-dir "$DIST_DIR" \
-  --version "$COREUTILS_VERSION" \
-  --install-dir "$WORK_ROOT" \
-  --target "$TARGET" \
-  --prefix "/usr"
-
-python3 \
-  "$SOURCE_DIR/diffutils.py" \
-  --dist-dir "$DIST_DIR" \
-  --version "$DIFFUTILS_VERSION" \
-  --install-dir "$WORK_ROOT" \
-  --target "$TARGET" \
-  --prefix "/usr"
-
-python3 \
-  "$SOURCE_DIR/file.py" \
-  --dist-dir "$DIST_DIR" \
-  --version "$FILE_VERSION" \
-  --install-dir "$WORK_ROOT" \
-  --target "$TARGET" \
-  --prefix "/usr"
-
-python3 \
-  "$SOURCE_DIR/findutils.py" \
-  --dist-dir "$DIST_DIR" \
-  --version "$FINDUTILS_VERSION" \
-  --install-dir "$WORK_ROOT" \
-  --target "$TARGET" \
-  --prefix "/usr" \
-  --localstatedir "/var/lib/located"
